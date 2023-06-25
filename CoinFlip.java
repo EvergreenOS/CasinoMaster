d667b349813a8f179e5791e4ca16c6a057514ed2
Evergreen <asgoodasnewa@gmail.com>
2023-06-26 06:57:22 +0900

    Start development of first gamble
    Basic coin flip to test interface, organically earning money and such.

---
+++ CoinFlip.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+/**
+ * Write a description of class YesOrNo here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class CoinFlip
+{
+    public CoinFlip()
+    {
+        
+    }
+    public void main()
+    {
+        System.out.print("Heads or tails?");
+        System.out.println("*****");
+        
+        Main main = new Main();
+        main.menu();
+    }
+}
