ddd7a8089a75274e31f1342d6978352822088247
Evergreen <asgoodasnewa@gmail.com>
2023-08-06 03:18:28 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,155 +1,87 @@
 import java.util.Scanner;
 /**
- * Acts as the main menu and hub for the flow of the overall game.
+ * Starts program and creates player.
+ *
+ * @author (Evergreen)
+ * @version (8/6/2023)
  */
 public class Main
 {
-    public static int day;
-    public static int hour;
-    public static Player player;
-    boolean game = true;
+    int day = 0;
+    public static int hour = 0;
+    public Player player;
     Scanner in = new Scanner(System.in);
-
+    
     public Main()
     {
+        System.out.println("Welcome to CasinoMaster!");
+        System.out.println("What is your name?");
+        System.out.println("");
         
-    }
-    
-    public void start()
-    {
-        day = 1;
-        hour = 0;
-        game = true;
-        System.out.println("Hello! Welcome to Casino Master.");
-        System.out.println("Please enter your name here: ");
         String name = in.nextLine();
         player = new Player(name);
-        System.out.println("*****");
+        
+        System.out.println("-----");
         menu();
     }
-
+    
     public void menu()
     {
-        // User interface
-        System.out.println("Type the word in the parentheses in order to access the respective section.");
-        System.out.println("Check Stats || (stat)");
-        System.out.println("Coin Flip || (coin)");
-        System.out.println("*****");
-        String decision = in.nextLine();
-
-        //Hub
-        if (decision.equals("stat"))
-        {
-            print();
-        }
-        if (decision.equals("coin"))
-        {
-            CoinFlip game = new CoinFlip();
-            game.main(player);
-        }
-        if (decision.equals("dev"))
-        {
-            dev();
-        }
-    }
-
-    public void dev()
-    {
-        System.out.println("Type the word in the parentheses in order to access the respective section.");
-        System.out.println("Increase Time || (t!)");
-        System.out.println("Set Money || (m!)");
-        System.out.println("Set Energy || (e!)");
-        System.out.println("Return to Main Menu || (return)");
-        System.out.println("*****");
-        String decision = in.nextLine();
-
-        if (decision.equals("t!"))
-        {
-            hour = 0;
-            increaseDay();
-            if (game == true)
-            {
-                dev();
-            }
-        }
-        if (decision.equals("m!"))
-        {
-            System.out.println("Set money amount: ");
-            Integer set = Integer.parseInt(in.nextLine());
-            player.money = set;
-            System.out.println("*****");
-            dev();
-        }
-        if (decision.equals("e!"))
-        {
-            System.out.println("Set energy amount: ");
-            Integer set = Integer.parseInt(in.nextLine());
-            player.energy = set;
-            System.out.println("*****");
-            dev();
-        }
-        if (decision.equals("return"))
-        {
-            menu();
-        }
-    }
-
-    public void print()
-    {
-        player.print();
-        System.out.println("*****");
-        menu();
-    }
-
-    public void increaseHour(int hours)
-    {
-        hour += hours;
-        checkEnergy();
         if (hour >= 24)
         {
-            hour -= 24;
-            day ++;
             increaseDay();
         }
-    }
-
-    public void increaseDay()
-    {
-        checkEnergy();
-        checkMoney();
-        if (game)
-        {
-            day++;
-            System.out.println("You are now on Day " + day + ".");
-            System.out.println("*****");
-        }
-    }
-
-    public void checkEnergy()
-    {
-        if (player.energy <= 0)
-        {
-            System.out.println("You ran out of energy!");
-            System.out.println("You collapsed on the floor and someone robbed you.");
-            System.out.println("*****");
-            player.money = (int)((double)(player.money / 10) * 5 * Math.random());
-        }
-    }
-
-    public void checkMoney()
-    {
         if (player.money <= 0)
         {
             gameOver();
         }
+        System.out.println("Choose what you wish to do by typing in the lettered");
+        System.out.println("command corresponding to each option.");
+        System.out.println("");
+        
+        System.out.println("Quit game ('x')");
+        System.out.println("See stats ('z')");
+        System.out.println("");
+        
+        String option = in.nextLine();
+        if (option.equals("x"))
+        {
+            System.out.println("You have voluntarily quit this game.");
+            gameOver();
+        }
+        if (option.equals("z"))
+        {
+            System.out.println("-----");
+            System.out.println("NAME: " + player.name);
+            System.out.println("MONEY: " + player.money);
+            System.out.println("ENERGY: " + player.energy);
+            System.out.println("-----");
+            menu();
+        }
     }
-
+    
+    public void increaseDay()
+    {
+        hour -= 24;
+        day ++;
+        
+        System.out.println("It is currently day " + day + ".");
+        System.out.println("You currently have " + player.money + "$.");
+        System.out.println("You currently have " + player.energy + " energy.");
+        System.out.println("");
+        
+        System.out.println("The time is " + hour + ":00 on day" + day + ".");
+        
+        System.out.println("-----");
+        menu();
+    }
+    
     public void gameOver()
     {
-        System.out.println("GAME OVER");
-        System.out.println("You came out with " + player.money + "$.");
-        System.out.println("You survived until day " + day + ".");
-        System.out.println("*****");
-        game = false;
+        System.out.println("You have no money and therefore lose.");
+        System.out.println("You have survived until day " + day + ".");
+        System.out.println("Thanks for playing!");
+        
+        System.out.println("-----");
     }
 }
